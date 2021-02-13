9136e6060d210be0233f9c376ae19e160aa1845b
Alexander <sphc@yandex.ru>
2021-02-13 18:43:38 +0300

    release version 1.2.41 with kotlin 1.4.30 coroutines-android 1.4.2

--- buildSrc/src/main/kotlin/appdependencies/Versions.kt
+++ buildSrc/src/main/kotlin/appdependencies/Versions.kt
@@ -2,8 +2,8 @@ package appdependencies
 
 object Versions {
 
-    const val gradle = "4.0.1"
-    const val kotlin = "1.4.10"
+    const val gradle = "4.1.2"
+    const val kotlin = "1.4.30"
     const val kotlinGradle = "1.3.6"
     const val anko_version = "0.10.8"
     const val google = "4.3.3"
@@ -11,10 +11,10 @@ object Versions {
     const val bintray = "1.8.4"
     const val maven = "2.0"
 
-    const val appcompat = "1.1.0"
-    const val appCoreX = "1.3.0"
+    const val appcompat = "1.2.0"
+    const val appCoreX = "1.3.2"
     const val constraintLayout = "1.1.3"
-    const val kotlinCoroutinesVersion = "1.3.9"
+    const val kotlinCoroutinesVersion = "1.4.2"
 
     /* test */
     const val junit = "4.12"
