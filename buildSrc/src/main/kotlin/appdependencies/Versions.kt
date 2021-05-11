ea8fb4a19b0e99bcbde1e35e3f83d27df05d0b3f
Alexander <sphc@yandex.ru>
2021-05-11 12:20:46 +0300

    release version 1.3.1 with kotlin 1.5.0 coroutines-android 1.5.0-RC

--- buildSrc/src/main/kotlin/appdependencies/Versions.kt
+++ buildSrc/src/main/kotlin/appdependencies/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
 
     const val appcompat = "1.2.0"
     const val appCoreX = "1.3.2"
-    const val constraintLayout = "1.2.0"
+    const val constraintLayout = "2.0.4"
     const val kotlinCoroutinesVersion = "1.5.0-RC"
 
     /* test */
