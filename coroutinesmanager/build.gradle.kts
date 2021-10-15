8d9b9af8871af98f34804e6082c6ce765982ecb8
Alexander <sphc@yandex.ru>
2021-10-15 11:20:02 +0300

    release version 1.3.6 with kotlin 1.5.31 coroutines-android 1.5.2 fix gradle.properties

--- coroutinesmanager/build.gradle.kts
+++ coroutinesmanager/build.gradle.kts
@@ -30,7 +30,7 @@ sourceSets {
 dependencies {
     implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
     implementation(kotlin("stdlib-jdk8", Versions.kotlin))
-    implementation(Libs.Core.kotlinxCoroutinesAndroid)
+    api(Libs.Core.kotlinxCoroutinesAndroid)
 }
 
 java {
