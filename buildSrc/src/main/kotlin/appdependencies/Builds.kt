aa9bb6ff63ffee75bbec2fe5459276f529121ec9
Alexander <sphc@yandex.ru>
2022-05-27 10:51:15 +0300

    release version 1.4.1 with kotlin 1.6.21 coroutines-android 1.6.1

--- buildSrc/src/main/kotlin/appdependencies/Builds.kt
+++ buildSrc/src/main/kotlin/appdependencies/Builds.kt
@@ -4,7 +4,7 @@ object Builds {
     const val MIN_VERSION = 19
     const val COMPILE_VERSION = 31
     const val TARGET_VERSION = 31
-    const val BUILD_TOOLS = "30.0.3"
+    const val BUILD_TOOLS = "31.0.0"
     const val APP_ID = "com.mincor.kotlincoroutinesmanager"
 
     object App {
@@ -13,7 +13,7 @@ object Builds {
     }
 
     object Manager {
-        const val VERSION_NAME = "1.4.0"
+        const val VERSION_NAME = "1.4.1"
     }
 }
 
--- buildSrc/src/main/kotlin/appdependencies/Versions.kt
+++ buildSrc/src/main/kotlin/appdependencies/Versions.kt
@@ -2,13 +2,13 @@ package appdependencies
 
 object Versions {
 
-    const val gradle = "7.0.4"
-    const val kotlin = "1.6.10"
+    const val gradle = "7.2.1"
+    const val kotlin = "1.6.21"
 
     const val appcompat = "1.3.1"
-    const val appCoreX = "1.6.0"
-    const val constraintLayout = "2.1.0"
-    const val kotlinCoroutinesVersion = "1.6.0"
+    const val appCoreX = "1.8.0-rc02"
+    const val constraintLayout = "2.1.4"
+    const val kotlinCoroutinesVersion = "1.6.1"
 
     /* test */
     const val junit = "4.13"
